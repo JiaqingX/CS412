58a1723b12dc53ae92ceb20de25da5ba08dc3e6d
JiaqingX <jiaqing@bu.edu>
2024-11-04 01:34:14 -0500

    debug and add urls

--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -11,15 +11,18 @@ urlpatterns = [
     path('profile/<int:pk>/', ShowProfilePageView.as_view(), name='show_profile'),
     path('create_profile/', CreateProfileView.as_view(), name='create_profile'),
 
+    # Status message related URLs
     path('status/create_status/<int:pk>/', CreateStatusMessageView.as_view(), name='create_status'),
+    path('status/<int:pk>/update/', UpdateStatusMessageView.as_view(), name='update_status'),
+    path('status/<int:pk>/delete/', DeleteStatusMessageView.as_view(), name='delete_status'),
+
+    # Profile related URLs
     path('profile/<int:pk>/update/', UpdateProfileView.as_view(), name='update_profile'),
     path('profile/<int:pk>/friend_suggestions/', ShowFriendSuggestionsView.as_view(), name='friend_suggestions'),
     path('profile/<int:pk>/news_feed/', ShowNewsFeedView.as_view(), name='news_feed'),
     path('profile/<int:pk>/add_friend/<int:other_pk>/', CreateFriendView.as_view(), name='add_friend'),
 
-    path('status/<int:pk>/update/', UpdateStatusMessageView.as_view(), name='update_status'),
-    path('status/<int:pk>/delete/', DeleteStatusMessageView.as_view(), name='delete_status'),
-
+    # Authentication URLs
     path('login/', auth_views.LoginView.as_view(template_name='mini_fb/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='mini_fb/logged_out.html'), name='logout'),
 ]
